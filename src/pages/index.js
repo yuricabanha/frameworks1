5c2d9e3aa26643f645cc37c6f4791bbb63f2618e
Yuri Cabanha <yh.yuricabanha@gmail.com>
2025-07-16 10:39:46 -0400

    prova2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Container, Row } from 'react-bootstrap';
 import Cards from './components/cards';
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch('https://portalnoticiasinfoa.vercel.app/api/noticias/apinoticias')
+  const res = await fetch('http://portalnoticiasyuri5ainfo.vercel.app/api/noticias/apinoticias')
   const repo = await res.json()
   // Pass data to the page via props
   return { props: { noticias: repo } }
