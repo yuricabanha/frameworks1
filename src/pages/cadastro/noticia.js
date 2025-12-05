de46d4ea025a210c14e9e7bb8839be04f0076143
Yuri Cabanha <yh.yuricabanha@gmail.com>
2025-12-05 10:10:58 -0400

    aula

--- src/pages/cadastro/noticia.js
+++ src/pages/cadastro/noticia.js
@@ -16,7 +16,7 @@ export default function Noticia() {
     };
 
     try {
-      const response = await fetch("https://g3-snowy.vercel.app/postnoticias", {
+      const response = await fetch("https://fw2api.vercel.app/postnoticias", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
